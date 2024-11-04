ac5b8a995ec4ed2c2044570fabf1fb0b969a8861
jesusinhoo <jesus.bojorquez1110@gmail.com>
2024-11-04 13:11:43 -0700

    Agregar archivos de proyecto.

---
+++ Proyecto_Colas/Cola.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Colas
+{
+    internal class Cola
+    {
+        private Nodo inicio;
+        private int contador;
+        private int max;
+
+        public Cola(int maxx)
+        {
+            max = maxx;
+            inicio = null;
+        }
+
+        private bool UnderFlow()
+        {
+            if (inicio == null)
+                return true;
+            else
+                return false;
+        }
+
+        private bool OverFlow()
+        {
+            if (contador == max)
+                return true;
+            else
+                return false;
+        }
+
+        public void Print()
+        {
+
+        }
+
+        public int Count()
+        {
+
+        }
+
+        public bool Insert(int num)
+        {
+            //regresa true al inseertar exitosamente
+            //Regresa false si la cola esta llena y no se pudo insertar
+        }
+
+        public int Extract()
+        {
+            //Regresa el valor extraido de la cola
+            //Si la cola esta vacia regresa -1 por que no pudo extraer
+        }
+
+
+
+    }
+}
---
+++ Proyecto_Colas/Nodo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Colas
+{
+    internal class Nodo
+    {
+        private int valor;
+        private Nodo sig;
+
+        public Nodo(int valorr)
+        {
+            valor = valorr;
+            sig = null;
+        }
+
+        public int Valor { get => valor; set => valor = value; }
+        internal Nodo Sig { get => sig; set => sig = value; }
+    }
+}
